fa3d08c38b6c81a8f3924c982b3ac02992aca287
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 03:26:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker(name)
+  name = "Hello, my name is Arel."
+  end
+def batch_badge_creator(attendees)
+    attendees=["Hello, my name is Edsger", "Hello, my name is, Ana", "Hello, my name is Charles", "Hello, me name is Alan", "Hello, my name is Grace", "Hello, my name is Linus", "Hello, my name is Matz" ] 
+    attendees.each do |badges| puts "Hello, my name is #{badges}. "
+  end
+end
