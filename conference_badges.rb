f0f30afb2f8868b6e5449ead324dec9a696a9d6c
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 03:27:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,25 @@
 def badge_maker(name)
-  name = "Hello, my name is Arel."
-  end
-def batch_badge_creator(attendees)
-    attendees=["Hello, my name is Edsger", "Hello, my name is, Ana", "Hello, my name is Charles", "Hello, me name is Alan", "Hello, my name is Grace", "Hello, my name is Linus", "Hello, my name is Matz" ] 
-    attendees.each do |badges| puts "Hello, my name is #{badges}. "
-  end
+  return "Hello, my name is #{name}."
 end
+def batch_badge_creator(array)
+  new_array = []
+  array.each{|name| new_array << "Hello, my name is #{name}."}
+  new_array
+  end
+   def assign_rooms(speakers)
+    array = []
+     room = 1
+     speakers.each do |name|
+       array.push("Hello, #{name}! You'll be assigned to room #{room}!")
+     room += 1
+   end
+     array
+ end
+ def printer(attendees)
+   batch_badge_creator(attendees).each do |badge|
+     puts badge
+ end
+   assign_rooms(attendees).each do |array|
+     puts array
+   end
+ end
